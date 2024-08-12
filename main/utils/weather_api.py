fa3af827c325808779e019c33ead56292c196e3d
bwjson <bwjson27@gmail.com>
2024-08-12 16:58:07 +0500

    api by name, api by coord

---
+++ main/utils/weather_api.py
@@ -0,0 +1,76 @@
+import requests
+import json
+
+API_KEY = '4fb58853c7917f3aa844a78cd28a80aa'
+
+class Weather_API:
+    def __init__(self, temp, weather, wind_speed, humidity):
+        self.temp = temp
+        self.weather = weather
+        self.wind_speed = wind_speed
+        self.humidity = humidity
+
+    @staticmethod
+    def get_weather_data(lat, lon, units='metric', index=0):
+        params = {
+            'lat': lat,
+            'lon': lon,
+            'units': units,
+            'appid': API_KEY
+        }
+
+        try:
+            response = requests.get('https://api.openweathermap.org/data/2.5/forecast', params=params, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+
+            weather_data = {
+                'temp': round(data['list'][index]['main']['temp']),
+                'weather': data['list'][index]['weather'][0]['main'],
+                'wind_speed': data['list'][index]['wind']['speed'],
+                'humidity': data['list'][index]['main']['humidity']
+            }
+
+            return weather_data
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data from OpenWeatherMap: {e}")
+            return None
+
+    @staticmethod
+    def get_city_weather_data(city, units='metric', index=0):
+        params = {
+            'q': city,
+            'units': units,
+            'appid': API_KEY
+        }
+
+        try:
+            response = requests.get('https://api.openweathermap.org/data/2.5/forecast', params=params, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+
+            if data['message'] == 0:
+                pass
+            else:
+                return 'City not found'
+
+            weather_data = {
+                'temp': round(data['list'][index]['main']['temp']),
+                'weather': data['list'][index]['weather'][0]['main'],
+                'wind_speed': data['list'][index]['wind']['speed'],
+                'humidity': data['list'][index]['main']['humidity']
+            }
+
+            return weather_data
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data from OpenWeatherMap: {e}")
+            return None
+
+    def __str__(self):
+        return f'Temperature: {self.temp}°C, Weather: {self.weather}, Wind Speed: {self.wind_speed} m/s, Humidity: {self.humidity}%'
+
+
+a = Weather_API.get_city_weather_data('Astana')
+print(a)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,38 @@
 from django.views.generic import TemplateView
+from .utils import weather_api as w
 
 
 class WeatherView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'main/weather.html'
 
     def get_context_data(self, **kwargs):
+        # date = self.request.GET.get('date', None)
+
+        search_city = self.request.GET.get('city', None)
+
+        if search_city is None:
+            current_data = w.Weather_API.get_weather_data(53.2194, 63.6354, index=0)
+            tomorrow_data = w.Weather_API.get_weather_data(53.2194, 63.6354, index=8)
+            day_after_data = w.Weather_API.get_weather_data(53.2194, 63.6354, index=16)
+
+            current = w.Weather_API(**current_data)
+            tomorrow = w.Weather_API(**tomorrow_data)
+            day_after = w.Weather_API(**day_after_data)
+        else:
+            search_city_data_c = w.Weather_API.get_city_weather_data(search_city, index=0)
+            search_city_data_t = w.Weather_API.get_city_weather_data(search_city, index=8)
+            search_city_data_d = w.Weather_API.get_city_weather_data(search_city, index=16)
+            current = w.Weather_API(**search_city_data_c)
+            tomorrow = w.Weather_API(**search_city_data_t)
+            day_after = w.Weather_API(**search_city_data_d)
+
         context = super().get_context_data(**kwargs)
         context['title'] = 'Weather'
+        context['weather'] = {
+            'current': current,
+            'tomorrow': tomorrow,
+            'day_after': day_after,
+        }
         return context
+
+
