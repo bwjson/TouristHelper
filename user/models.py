23ea2c0144671fbdae8d7f1ab208bed3412f404d
bwjson <bwjson27@gmail.com>
2024-08-19 15:58:27 +0500

    extending user model, profile update

--- user/models.py
+++ user/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, null=True, verbose_name='Avatar')
+    date_of_birth = models.DateTimeField(null=True, blank=True, verbose_name='Date of Birth')
